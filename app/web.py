7b2eedc86371e85938c27aad3ede74f118f36a2e
Evelin Evlogiev <evelin.evlogiev@gmail.com>
2024-01-22 15:34:29 +0200

    Added pylint and black formatter

--- app/web.py
+++ app/web.py
@@ -10,19 +10,19 @@ env = os.getenv('ENV')
 @app.route("/")
 def home():
     return f"""
-    <html>
-    <head>
-        <title>Flask App</title>
-    </head>
-    <body>
-        <center>
-            <h3>Hello, DevOps Gurus!<br>
-                You are connected to <span style="color: red;">{env}</span> environment. OK!?</h3>
-            <img border="0" src="/static/devops.png" alt="devops" width="1000" height="500" align="middle"/>
-        </center>
-    </body>
-    </html>
-    """
+<html>
+<head>
+    <title>Flask App</title>
+</head>
+<body>
+    <center>
+        <h3>Hello, DevOps Gurus!<br>
+            You are connected to <span style="color: red;">{env}</span> environment. OK!?</h3>
+        <img border="0" src="/static/devops.png" alt="devops" width="1000" height="500" align="middle"/>
+    </center>
+</body>
+</html>
+"""
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
