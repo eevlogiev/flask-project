9977046e4f0863155966d810ba7ba0caf04bc884
Evelin Evlogiev <evelin.evlogiev@gmail.com>
2024-01-22 19:41:37 +0200

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,13 +4,6 @@ locals {
   region    = "us-east-1"
   env       = "dev"
 
-  #  tags = {
-  #    environment                                   = local.env
-  #    team                                          = "devops"
-  #    "kubernetes.io/role/elb"                      = 1
-  #    "kubernetes.io/cluster/${local.name}-cluster" = "owned"
-  #  }
-
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
