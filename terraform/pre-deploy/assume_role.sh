2e9d6cbf6f8cc3c47902c30709918c84d7fce0f1
Evelin Evlogiev <evelin.evlogiev@gmail.com>
2024-01-27 00:43:15 +0200

    Deploy to Staging

--- terraform/pre-deploy/assume_role.sh
+++ terraform/pre-deploy/assume_role.sh
@@ -1,4 +1,4 @@
-OUT=$(aws sts assume-role --role-arn arn:aws:iam::776410366842:role/eks-role --role-session-name AWSCLI-Session) \
+OUT=$(aws sts assume-role --role-arn arn:aws:iam::361295318532:role/eks-role --role-session-name AWSCLI-Session) \
 && export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r .Credentials.AccessKeyId) \
 && export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r .Credentials.SecretAccessKey) \
 && export AWS_SESSION_TOKEN=$(echo $OUT | jq -r .Credentials.SessionToken) \
