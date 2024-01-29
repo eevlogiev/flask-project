b58fd386ca8f97bcc1e5a1e042c457d9d3e19c17
Evelin Evlogiev <evelin.evlogiev@gmail.com>
2024-01-29 17:24:40 +0200

    Deploy to dev environment

--- terraform/pre-deploy/assume_role.sh
+++ terraform/pre-deploy/assume_role.sh
@@ -1,4 +1,4 @@
-OUT=$(aws sts assume-role --role-arn arn:aws:iam::920440469323:role/eks-role --role-session-name AWSCLI-Session) \
+OUT=$(aws sts assume-role --role-arn arn:aws:iam::156062818068:role/eks-role --role-session-name AWSCLI-Session) \
 && export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r .Credentials.AccessKeyId) \
 && export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r .Credentials.SecretAccessKey) \
 && export AWS_SESSION_TOKEN=$(echo $OUT | jq -r .Credentials.SessionToken) \
